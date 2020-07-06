f3b9c551e7709e15f53fd4aa5ce054301ac710ae
Caleb Waters <cw@mekanation.com>
2020-07-06 18:39:23 -0500

    Updated sprites folder to use a single spritemap instead of multiple img files.

--- Trollaga/core/src/com/mygdx/trollaga/TrollagaGame.java
+++ Trollaga/core/src/com/mygdx/trollaga/TrollagaGame.java
@@ -3,31 +3,36 @@ package com.mygdx.trollaga;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class TrollagaGame extends ApplicationAdapter {
+	Sprite hero;
 	SpriteBatch batch;
-	Texture img;
+	TextureAtlas textureAtlas;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		textureAtlas = new TextureAtlas("sprites.txt");
+		hero = textureAtlas.createSprite("sprite7");
+		batch= new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		hero.draw(batch);
 		batch.end();
+
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+		textureAtlas.dispose();
 	}
 }
